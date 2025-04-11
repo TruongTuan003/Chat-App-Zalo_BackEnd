49dd6eb3f0d432b2c915d35865d34d6755870249
TruongTuan003 <21030271.tuan@student.iuh.edu.vn>
2025-04-11 21:22:34 +0700

    add Update User detail

---
+++ controller/updateUserDetails.js
@@ -0,0 +1,47 @@
+const getUserDetailFromToken = require("../helpers/getUserDetailFromToken");
+const UserModel = require("../models/UserModel");
+
+async function updateUserDetails(request, response) {
+  try {
+    const token = request.cookies.token || "";
+    const user = await getUserDetailFromToken(token);
+
+    if (!user) {
+      return response.status(401).json({
+        message: "Unauthorized. Invalid or missing token",
+        error: true,
+      });
+    }
+
+    const { name, profile_pic } = request.body;
+
+    if (!name) {
+      return response.status(400).json({ message: "Name is required" });
+    }
+
+    await UserModel.updateOne(
+      { _id: user._id },
+      {
+        name,
+        profile_pic,
+      }
+    );
+
+    const userInformation = await UserModel.findById(user._id).select(
+      "-password"
+    );
+
+    return response.json({
+      message: "User updated successfully",
+      data: userInformation,
+      success: true,
+    });
+  } catch (error) {
+    return response.status(500).json({
+      message: error.message || "Something went wrong",
+      error: true,
+    });
+  }
+}
+
+module.exports = updateUserDetails;
